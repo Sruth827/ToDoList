c89f9d13742b422efb1e87d0f78aa7ed591b6305
Sruth827 <Sruth827@gmail.com>
2025-03-14 21:02:25 -0700

    Update DoTaskItem.java

--- src/application/DoTaskItem.java
+++ src/application/DoTaskItem.java
@@ -2,53 +2,60 @@ package application;
 
 import java.time.LocalDateTime;
 
-
-abstract class task {
-	private LocalDateTime dateEntered; 
+abstract class Task implements TaskSelectionListener {
+	
 	private String name;
 	private String description;
 	private Integer minutesRequired;
+	private Integer taskPriority; 
 	
-	public task(LocalDateTime dateEntered, String name, String description, Integer minutesRequired) {
-		this.dateEntered = dateEntered;
+	public task( String name, String description, Integer minutesRequired) {
+		if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        if (description == null || description.isEmpty()) {
+            throw new IllegalArgumentException("Description cannot be null or empty");
+        }
+        if (minutesRequired == null || minutesRequired <= 0) {
+            throw new IllegalArgumentException("Minutes required must be positive");
+        }
 		this.name = name; 
-		this.description = name; 
+		this.description = description; 
 		this.minutesRequired = minutesRequired;
 	}
+	
 }
 
-class DoTaskItem extends task{
+class DoTaskItem extends Task{
 
-	public DoTaskItem(LocalDateTime dateEntered, String name, String description , Integer minutesRequired) {
-		super(dateEntered, name, description, minutesRequired);
-		// TODO Auto-generated constructor stub
+	public DoTaskItem(String name, String description , Integer minutesRequired) {
+		super(name, description, minutesRequired);
 	}
 
+
 }
 
-class ScheduleTaskItem extends task {
+class ScheduleTaskItem extends Task {
 
-	public ScheduleTaskItem(LocalDateTime dateEntered, String name, String description, Integer minutesRequired) {
-		super(dateEntered, name, description, minutesRequired);
-		// TODO Auto-generated constructor stub
+	public ScheduleTaskItem(String name, String description, Integer minutesRequired) {
+		super(name, description, minutesRequired);
 	}
 	
 }
 
 class DelegateTaskItem extends task {
 
-	public DelegateTaskItem(LocalDateTime dateEntered, String name, String description, Integer minutesRequired) {
-		super(dateEntered, name, description, minutesRequired);
-		// TODO Auto-generated constructor stub
+	public DelegateTaskItem(String name, String description, Integer minutesRequired) {
+		super(name, description, minutesRequired);
 	}
 	
 }
 
 class DeleteTaskItem extends task{
 
-	public DeleteTaskItem(LocalDateTime dateEntered, String name, String description, Integer minutesRequired) {
-		super(dateEntered, name, description, minutesRequired);
-		// TODO Auto-generated constructor stub
+	public DeleteTaskItem(String name, String description, Integer minutesRequired) {
+		super(name, description, minutesRequired);
 	}
 	
 }
+

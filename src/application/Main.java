ec75e12d5ec1a988ecf1c2032d219fb5d2c6e64f
Sruth827 <Sruth827@gmail.com>
2025-03-14 21:02:39 -0700

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,18 +3,29 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.VBox;
+import javafx.fxml.*;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = FXMLLoader.load(getClass().getResource("ToDo.fxml"));
+			
+			TaskManager taskManager = new TaskManager(); 
+			
+			MyController controller = new MyController(); 
+			controller.setTaskSelectionListener(taskManager);
+			loader.setController(controller);
+			
+			VBox root = loader.load();
+			
+			Scene scene = new Scene(root,900,600);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Eisenhower Matrix");
 			primaryStage.setScene(scene);
-			primaryStage.show();
+			primaryStage.show(); 
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}

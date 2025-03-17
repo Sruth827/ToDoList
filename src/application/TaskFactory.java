507c87e743bc15acf109e43499d8f2e362b46d04
Sruth827 <150305715+Sruth827@users.noreply.github.com>
2025-03-16 20:39:17 -0700

    Update TaskFactory.java

--- src/application/TaskFactory.java
+++ src/application/TaskFactory.java
@@ -2,7 +2,7 @@ package application;
 
 public class TaskFactory {
 
-    public static task createTask(String name, String description, Integer minutesRequired, int selectTaskID) {
+    public static Task createTask(String name, String description, Integer minutesRequired, int selectTaskID) {
         return switch (selectTaskID) {
             case 1 -> new DoTaskItem(name, description, minutesRequired);
             case 2 -> new ScheduleTaskItem(name, description, minutesRequired);
@@ -11,5 +11,5 @@ public class TaskFactory {
             default -> throw new IllegalArgumentException("Invalid Task ID");
         };
     }
-}
- 
+} 
+

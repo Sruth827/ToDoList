f30fad5214c1373e04f96d97c965bf69e76d160f
Sruth827 <150305715+Sruth827@users.noreply.github.com>
2025-03-16 20:39:14 -0700

    Create TaskCellFactory.java

---
+++ src/application/TaskCellFactory.java
@@ -0,0 +1,44 @@
+package application;
+
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
+public class TaskCellFactory implements Callback<ListView<Task>, ListCell<Task>> {
+    @Override
+    public ListCell<Task> call(ListView<Task> param) {
+        return new ListCell<>(){
+            @Override
+            public void updateItem(Task task, boolean empty) {
+                super.updateItem(task, empty);
+                if (empty || task == null) {
+                    setText(null);
+                    setStyle(null);
+                } else {
+                    // Set task details
+                    setText(task.getName() + " / " + task.getMinutesRequired() + " / " + task.getDescription());
+
+                    // Apply styles based on task priority
+                    switch (task.getTaskPriority()) {
+                        case 1:
+                            setStyle("-fx-background-color: lightgreen; -fx-font-weight: bold" );
+                            break;
+                        case 2:
+                            setStyle("-fx-background-color: yellow;  -fx-font-weight: bold");
+                            break;
+                        case 3:
+                            setStyle("-fx-background-color: orange;  -fx-font-weight: bold");
+                            break;
+                        default:
+                            setStyle("-fx-background-color: red;  -fx-font-weight: bold");
+                            break;
+                    }
+                    
+                    if(this.isSelected()) {
+                    	setStyle("-fx-background-color: black; -fx-text-fill: white; -fx-font-style: italic");
+                    }
+                }
+            }
+        };
+    }
+}
